ff566fd8dbdcc934875abf169c0a76a79a319e00
Rogerio Messias <rp_messias@yahoo.com.br>
2024-03-02 16:48:42 -0300

    exclusao

---
+++ Alura/Sabor-express.py
@@ -0,0 +1,9 @@
+print("Sabor EXPRESS\n")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opcao: ')
+print(f'Você escolheu a opcao {opcao_escolhida}')
